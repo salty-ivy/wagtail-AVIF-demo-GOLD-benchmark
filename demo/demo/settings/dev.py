8cf7523d9cd57571cee5356530e9024f5927ae2f
Aman Pandey <aman2001mi@gmail.com>
2023-08-13 00:06:21 +0530

    project setup

---
+++ demo/demo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-q1t0r0=)en%(+xmr-61e7x^n&6q-)5js!eo(=fmdz(=st&5x#5"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
